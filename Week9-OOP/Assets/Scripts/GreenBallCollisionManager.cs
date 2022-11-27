55a327e2ddc9078d81d92a7ec4334872651557d3
netnicky <asherk@udel.edu>
2022-11-26 22:14:27 -0500

    week 9 hw
    added my week 9 homework

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject greenBall;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        if(OtherBall.GetComponent<RedBallCollisionManager>() != null)
+        {
+            Debug.Log("Green collided with Red");
+
+            //the green ball grows
+            greenBall.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+}
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public GameObject redBall;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        if(OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+        {
+            Debug.Log("Red collided with yellow");
+            //the red ball teleports in the air to the center
+            redBall.transform.position = new Vector3(0, 5, 0);
+
+        }
+    }
+}
+
